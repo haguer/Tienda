5049b890162ff8454d94a4b40328c7aaa13e1b07
Henry Aldemar Guerrero Erazo <henry.guerrero@unad.edu.co>
2020-05-13 19:30:14 -0500

    Adicion de productscontroller

---
+++ Tienda.Web/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Tienda.Web.Data.Entities
+{
+
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+    }
+}
